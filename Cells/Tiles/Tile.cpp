48228239904dad930e22b0dc07fd5c57001acd55
Baptiste-Crepin <81614462+Nypec@users.noreply.github.com>
2023-11-03 12:45:51 +0100

    up

--- Cells/Tiles/Tile.cpp
+++ Cells/Tiles/Tile.cpp
@@ -4,11 +4,11 @@
 
 #include "Tile.h"
 
-Tile::Tile(int x, int y) : x(x), y(y) {}
-Tile::~Tile() {}
-
-int Tile::getX() const {return x;}
-int Tile::getY() const {return y;}
-
-void Tile::setX(int x) {Tile::x = x;}
-void Tile::setY(int y) {Tile::y = y;}
+//Tile::Tile(int x, int y) : x(x), y(y) {}
+//Tile::~Tile() {}
+//
+//int Tile::getX() const {return x;}
+//int Tile::getY() const {return y;}
+//
+//void Tile::setX(int x) {Tile::x = x;}
+//void Tile::setY(int y) {Tile::y = y;}
