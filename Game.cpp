28fe0c5bb0387dfc7ff25a03526f09f99c71eaf5
Baptiste-Crepin <81614462+Nypec@users.noreply.github.com>
2023-11-18 22:03:39 +0100

    order of actions

--- Game.cpp
+++ Game.cpp
@@ -48,14 +48,15 @@ void Game::newTurn() {
     do {
         cout << this->getCurrentPlayer().getId() << " | " << endl;
         this->getBoard().printBoard();
-        
         cout << "Queue : " << endl;
         this->getTileQueue().displayQueue();
-        this->setNextPlayer();
 
         //todo: ask player if he wants to exchange a tile (if he has a ticket to exchange)
         cout << tileQueue.tileExchange().getId() << endl;
         tileQueue.nextTile();
+
+        this->setNextPlayer();
+
 
     } while ((this->getCurrentPlayerIndex() < this->getPlayerCount()));
 
